b8b380500dac90778339fbcfd3845f9c86cd0998
male <male@example.com>
2013-05-16 13:50:45 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Latihan::Application.config.secret_token = '5bab5d07330c7115fc1ff0e5a15e365d08784958715f4d3a460a494a603cd64a400ab1136e6b762bf4627fb30bb649126dafdfaf824c0a007626d6811b4791c8'
